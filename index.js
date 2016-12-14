6611107564d52e79605d6407047a6b1989a06571
rigor789 <rigor789@gmail.com>
2016-12-14 21:55:37 +0000

    Ditch jQuery

--- index.js
+++ index.js
@@ -1,30 +1,96 @@
-'use strict';
+'use strict'
 
 var $ = require('jquery')
+var BezierEasing = require('bezier-easing')
 
-exports.install = function(Vue) {
-  function handleClick(e) {
-    e.preventDefault()
-    var page = $('html, body')
-    var events = 'scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove'
-    page.on(events, function() {
-      page.stop()
-    });
-
-    page.animate({
-      scrollTop: $(this.value).offset().top + 'px'
-    }, 300, function() {
-      page.off(events)
-    })
-  }
-
-
-  Vue.directive('scroll-to', {
-    bind: function(el, binding) {
-      el.addEventListener('click', handleClick.bind(binding))
+var _ = exports.utils = {
+    $: function (selector) {
+        return document.querySelector(selector)
     },
-    unbind: function(el) {
-      el.removeEventListener('click', handleClick)
+    on: function (element, events, handler) {
+        if (!(events instanceof Array)) {
+            events = [events]
+        }
+        for (var i = 0; i < events.length; i++) {
+            element.addEventListener(events[i], handler)
+        }
+    },
+    off: function (element, events, handler) {
+        if (!(events instanceof Array)) {
+            events = [events]
+        }
+        for (var i = 0; i < events.length; i++) {
+            element.removeEventListener(events[i], handler)
+        }
     }
-  })
+}
+
+exports.install = function (Vue) {
+    function handleClick(e) {
+        e.preventDefault()
+
+        exports.scrollTo(this.value, 500, {
+            easing: exports.easing['ease']
+        })
+    }
+
+
+    Vue.directive('scroll-to', {
+        bind: function (el, binding) {
+            _.on(el, 'click', handleClick.bind(binding))
+        },
+        unbind: function (el) {
+            _.off(el, 'click', handleClick)
+        }
+    })
+}
+
+exports.scrollTo = function (element, duration, options) {
+    var page = _.$('html, body')
+    var events = ['scroll', 'mousedown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'keyup', 'touchmove']
+    var abort = false
+
+    var abortFn = function () {
+        abort = true
+    }
+
+    _.on(page, events, abortFn)
+
+    var initialY = window.pageYOffset
+    var elementY = initialY + _.$(element).getBoundingClientRect().top
+    var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+    var diff = targetY - initialY
+    var easing = BezierEasing.apply(BezierEasing, options.easing)
+    var start
+
+    var done = function () {
+        _.off(page, events, abortFn)
+    }
+
+    if (!diff) return
+
+    window.requestAnimationFrame(function step(timestamp) {
+        if (abort) return
+        if (!start) start = timestamp
+
+        var time = timestamp - start
+        var progress = Math.min(time / duration, 1)
+        progress = easing(progress)
+
+        window.scrollTo(0, initialY + diff * progress)
+
+        if (time < duration) {
+            window.requestAnimationFrame(step)
+        } else {
+            done()
+        }
+    })
+}
+
+exports.easing = {
+    'ease': [0.25, 0.1, 0.25, 1.0],
+    'linear': [0.00, 0.0, 1.00, 1.0],
+    'ease-in': [0.42, 0.0, 1.00, 1.0],
+    'ease-out': [0.00, 0.0, 0.58, 1.0],
+    'ease-in-out': [0.42, 0.0, 0.58, 1.0]
 }
