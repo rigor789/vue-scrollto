5db55401cff9972d3da0e89b9a4e8d0590caf144
Christian Stornowski <christian.stornowski@materna.de>
2019-09-17 10:05:09 +0200

    Added better types for typescript

--- vue-scrollto.d.ts
+++ vue-scrollto.d.ts
@@ -1,36 +1,39 @@
-import Vue, { DirectiveOptions, PluginObject } from "vue"
+import {DirectiveOptions, PluginObject} from "vue"
 
 type ElementDescriptor = Element | string
 
 export interface ScrollOptions {
-  container?: ElementDescriptor
-  duration?: number
-  easing?: string
-  offset?: number
-  force?: boolean
-  cancelable?: boolean
-  onStart?: false | ((element: Element) => any)
-  onDone?: false | ((element: Element) => any)
-  onCancel?: false | ((event: Event, element: Element) => any)
-  x?: boolean
-  y?: boolean
+    el?: ElementDescriptor;
+    element?: ElementDescriptor;
+    container?: ElementDescriptor;
+    duration?: number;
+    easing?: string | [number, number, number, number];
+    offset?: number | ((element: ElementDescriptor, container: ElementDescriptor) => number);
+    force?: boolean;
+    cancelable?: boolean;
+    onStart?: ((element: Element) => any) | false;
+    onDone?: ((element: Element) => any) | false;
+    onCancel?: ((event: Event, element: Element) => any) | false;
+    x?: boolean;
+    y?: boolean;
 }
 
-type ScrollToFunction = (
-  element: ElementDescriptor,
-  duration?: number,
-  options?: ScrollOptions,
-) => () => void
+type ScrollToFunction = {
+    (options: ScrollOptions): void;
+    (element: ElementDescriptor, options?: ScrollOptions): void;
+    (element: ElementDescriptor, duration: number, options?: ScrollOptions): void;
+}
 
 declare const _default: PluginObject<ScrollOptions> &
-  DirectiveOptions & {
+    DirectiveOptions & {
     scrollTo: ScrollToFunction
-  }
+    setDefaults: ScrollToFunction;
+};
 
 export default _default
 
 declare module "vue/types/vue" {
-  interface Vue {
-    $scrollTo: ScrollToFunction
-  }
+    interface Vue {
+        $scrollTo: ScrollToFunction
+    }
 }
